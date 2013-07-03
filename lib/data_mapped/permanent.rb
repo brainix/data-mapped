629e45880f8a2c99fc8b961e4dbf0a1e13cd2279
Raj Shah <brainix@gmail.com>
2013-07-03 10:04:44 -0700

    fleshed out DataMapped::Permanent mixin

--- lib/data_mapped/permanent.rb
+++ lib/data_mapped/permanent.rb
@@ -1,4 +1,22 @@
+require 'data_mapper'
+
 module DataMapped
   module Permanent
+
+    module InstanceMethods
+      def vomit_before_destruction
+        DataMapper::logger.error '[ERROR] permanent model resource cannot be deleted'
+        throw :halt
+      end
+    end
+
+    DataMapper::Model.append_inclusions(InstanceMethods)
+
+    def self.included(other)
+      other.class_eval do
+        before :destroy, :vomit_before_destruction
+      end
+    end
+
   end
 end
