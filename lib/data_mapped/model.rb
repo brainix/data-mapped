5d8b3ad915e1068276ad78807240641c0383ceda
Raj Shah <brainix@gmail.com>
2013-07-03 10:00:22 -0700

    fleshed out DataMapped::Model mixin

--- lib/data_mapped/model.rb
+++ lib/data_mapped/model.rb
@@ -1,4 +1,16 @@
+require 'data_mapper'
+
 module DataMapped
   module Model
+
+    def self.included(other)
+      other.class_eval do
+        include DataMapper::Resource
+        property :id, DataMapper::Property::Serial
+        property :created_at, DataMapper::Property::DateTime
+        property :updated_at, DataMapper::Property::DateTime
+      end
+    end
+
   end
 end
